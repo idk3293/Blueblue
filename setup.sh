c32b6179be73e69f0c617d0ea042d87769d12014
idk3293 <onlylovents+1@gmail.com>
2025-03-15 13:04:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -165,12 +165,7 @@ echo -e "$white\033[0;34m└─────────────────�
 sleep 1
 wget -q https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #install ins-xray
-echo -e "$white\033[0;34m┌─────────────────────────────────────────┐${NC}"
-echo -e " \E[41;1;39m            ⇱ Install Xray ⇲             \E[0m$NC"
-echo -e "$white\033[0;34m└─────────────────────────────────────────┘${NC}"
-sleep 1 
-wget -q https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
-wget -q https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+
 
 # // Download Data
 echo -e "${GREEN}Download Data${NC}"
